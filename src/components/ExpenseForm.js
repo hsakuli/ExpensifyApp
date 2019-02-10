8237f0bb48aef7a7b26fbe724fe2f2e85fdb7f62
Lucas <ilukash@mac.com>
2019-02-09 18:01:04 -0800

    final commit

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -57,18 +57,19 @@ export default class ExpenseForm extends React.Component {
 	}
 
 	render(){
-		return (
-			<div>
-				{this.state.error && <p>this.state.error</p>}
-				<form onSubmit={this.onSubmit}>
+		return (			
+				<form className="form" onSubmit={this.onSubmit}>
+					{this.state.error && <p className="form__error">this.state.error</p>}
 					<input
 						type='text'
 						placeholder='Description'
 						autoFocus
+						className="text-input"
 						value={this.state.description}
 						onChange={this.onDescriptionChange}
 					/>
 					<input
+						className="text-input"
 						type='number'
 						placeholder='Amount'
 						value={this.state.amount}
@@ -83,13 +84,15 @@ export default class ExpenseForm extends React.Component {
 						isOutsideRange={(day) => false}
 					/>
 					<textarea
+						className="textarea"
 						placeholder='Add a note for your expense (optional)'
 						value={this.state.note}
 						onChange={this.onNoteChange}
 					/>
-					<button>Add Expense</button>
+					<div>
+						<button className="button">Add Expense</button>
+					</div>
 				</form>
-			</div>
 		)
 	}
 }
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,8 +5,12 @@ import { startLogin } from '../actions/auth';
 
 
 export const LoginPage = ({startLogin}) => (
-	<div>
-		<button onClick={startLogin}>Login</button>
+	<div className="box-layout">
+		<div className="box-layout__box">
+			<h1 className="box-layout__title">Expensify</h1>
+			<p>Its time you reign in yeetus.</p>
+			<button className ="button"onClick={startLogin}>Login with Google</button>
+		</div>
 	</div>
 );
 
